45c7ae1967691d05ad8069f510a6c9ce14b62d38
linlin <llee3@caltech.edu>
2020-09-19 16:49:54 -0500

    Initial Commit

---
+++ Testing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testing
+//
+//  Created by Lin Lin Lee on 9/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Lin Lin Lee on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
