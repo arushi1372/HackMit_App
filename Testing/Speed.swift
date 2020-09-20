3535eddfb88714a47db81e392a3725b2ed66569b
Arushi <aagarwa5@caltech.edu>
2020-09-19 23:17:53 -0700

    added basic line graph

--- Testing/Speed.swift
+++ Testing/Speed.swift
@@ -8,6 +8,7 @@
 import Foundation
 import CoreLocation
 import Combine
+import UIKit
 
 class LocationManager: NSObject, ObservableObject {
 
@@ -26,6 +27,18 @@ class LocationManager: NSObject, ObservableObject {
     }
 
     @Published var lastLocation: CLLocation? {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    @Published var allLocation: [CLLocation]? {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    @Published var allSpeeds: [CGFloat]? {
         willSet {
             objectWillChange.send()
         }
@@ -61,6 +74,21 @@ extension LocationManager: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         self.lastLocation = location
+        self.allLocation = locations
+        var speeds: [CGFloat] = []
+        for index in 0..<locations.count{
+            var speed = locations[index].speed
+            speed = speed * (2.23694/3600) * (411) //convert meters per second to mile //convert to grams of CO2 emitted
+            speeds.append(CGFloat(speed))
+        }
+        
+        let chunkSize = 60
+        let chunks = stride(from: 0, to: speeds.count, by: chunkSize).map {
+            Array(speeds[$0..<min($0 + chunkSize, speeds.count)])
+        }
+        let summed = chunks.map { $0.reduce(0, {$0 + $1})}
+        self.allSpeeds = summed
+//        print(#function, allSpeeds)
         print(#function, location)
     }
 }
--- Testing/SpeedView.swift
+++ Testing/SpeedView.swift
@@ -9,18 +9,31 @@ import SwiftUI
 
 struct MovementView: View {
     @ObservedObject var locationManager = LocationManager()
-
-    var userSpeed: String {
-        return "\(locationManager.lastLocation?.speed ?? 0)"
+    @State var on = true
+    
+    var userSpeed: [CGFloat] {
+        return [4.7, 3.2, 5.6, 7.8, 9.0, 15]
+//        return locationManager.allSpeeds ?? [0.0]
     }
-
+    
     var body: some View {
         VStack {
-            Text("location status: \(locationManager.statusString)")
             HStack {
-                Text("Speed: \(userSpeed)")
+                Text("Line Graph")
                 .navigationBarTitle(Text("Movement Footprint"))
             }
+            LineGraph(dataPoints: userSpeed)
+                .trim(to: on ? 1 : 0)
+                .stroke(Color.red, lineWidth: 2)
+                .aspectRatio(16/9, contentMode: .fit)
+                .border(Color.gray, width: 1)
+                .padding()
+//                .withAnimation(.easeInOut(duration: 2))
+//            Button("Animate") {
+//                withAnimation(.easeInOut(duration: 2)){
+//                    self.on.toggle()
+//                }
+//            }
         }
     }
 }
@@ -30,4 +43,28 @@ struct MovementView_Previews: PreviewProvider {
         MovementView()
     }
 }
+
+struct LineGraph: Shape {
+    var dataPoints: [CGFloat]
+
+    func path(in rect: CGRect) -> Path {
+        func point(at ix: Int) -> CGPoint {
+            let point = dataPoints[ix]
+            let x = rect.width * CGFloat(ix) / CGFloat(dataPoints.count - 1)
+            let y = (rect.height - point)
+            return CGPoint(x: x, y: y)
+        }
+        
+        guard dataPoints.count > 1 else { return Path{_ in }}
+        let start = dataPoints[0]
+        print(dataPoints)
+        return Path { p in
+            p.move(to: CGPoint(x: 0, y: rect.height - start))
+            for idx in dataPoints.indices {
+                p.addLine(to: point(at: idx))
+            }
+        }
+    }
+}
+
 #endif
