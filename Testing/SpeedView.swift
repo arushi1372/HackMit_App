b3db05c54cd357a895bc4ae7d9b7621108521fbe
Arushi <aagarwa5@caltech.edu>
2020-09-20 00:36:43 -0700

    added some aesthetic

--- Testing/SpeedView.swift
+++ Testing/SpeedView.swift
@@ -12,28 +12,46 @@ struct MovementView: View {
     @State var on = true
     
     var userSpeed: [CGFloat] {
-        return [4.7, 3.2, 5.6, 7.8, 9.0, 15]
+        return [30, 3.2, 67, 48, 96, 25]
 //        return locationManager.allSpeeds ?? [0.0]
     }
     
+    var totalday: Double {
+        var total = 0.0
+        for index in 0..<userSpeed.count{
+            total += Double(userSpeed[index])
+        }
+        return total
+    }
+    
     var body: some View {
-        VStack {
-            HStack {
-                Text("Line Graph")
-                .navigationBarTitle(Text("Movement Footprint"))
+        ZStack{
+            Color.pink.opacity(0.25)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    Text("Your Carbon Footprint From Movement Over Time").fontWeight(.bold).font(.title)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 100.0)
+                        .navigationBarTitle(Text("Movement Footprint"))
+                }
+                LineGraph(dataPoints: userSpeed)
+                    .trim(to: on ? 1 : 0)
+                    .stroke(Color.green ,style: StrokeStyle(lineWidth: 3, lineJoin: .round))
+                    .padding(.bottom, 10.0)
+                    .aspectRatio(16/9, contentMode: .fit)
+                    .padding(.bottom, 100.0)
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.green.opacity(0.5), lineWidth: 2)
+                        .padding(.bottom, 100.0)
+                    )
+                HStack {
+                    Text("Total: \(Int(totalday))g CO2").fontWeight(.bold).font(.title)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 200.0)
+                }
             }
-            LineGraph(dataPoints: userSpeed)
-                .trim(to: on ? 1 : 0)
-                .stroke(Color.red, lineWidth: 2)
-                .aspectRatio(16/9, contentMode: .fit)
-                .border(Color.gray, width: 1)
-                .padding()
-//                .withAnimation(.easeInOut(duration: 2))
-//            Button("Animate") {
-//                withAnimation(.easeInOut(duration: 2)){
-//                    self.on.toggle()
-//                }
-//            }
         }
     }
 }
@@ -50,16 +68,16 @@ struct LineGraph: Shape {
     func path(in rect: CGRect) -> Path {
         func point(at ix: Int) -> CGPoint {
             let point = dataPoints[ix]
-            let x = rect.width * CGFloat(ix) / CGFloat(dataPoints.count - 1)
+            let x = rect.width * CGFloat(ix) / CGFloat(dataPoints.count + 1)
             let y = (rect.height - point)
-            return CGPoint(x: x, y: y)
+            return CGPoint(x: x+30, y: y)
         }
         
         guard dataPoints.count > 1 else { return Path{_ in }}
         let start = dataPoints[0]
         print(dataPoints)
         return Path { p in
-            p.move(to: CGPoint(x: 0, y: rect.height - start))
+            p.move(to: CGPoint(x: 30, y: rect.height - start))
             for idx in dataPoints.indices {
                 p.addLine(to: point(at: idx))
             }
